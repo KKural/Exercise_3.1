7feaac5501ebdf68ecb227549226f231e04482a8
KKural <122997366+KKural@users.noreply.github.com>
2025-06-05 15:50:09 +0200

    Update tests.R

--- descriptive-statistics/evaluation/tests.R
+++ descriptive-statistics/evaluation/tests.R
@@ -1,7 +1,7 @@
 context("Check correct answer for multiple choice")
 
 test_that("Answer must be option 2", {
-  result <- evalSource()
-  expect_equal(result, 2, 
+  result <- as.integer(evalSource())
+  expect_equal(result, 2,
                info = "Incorrect: The main purpose of descriptive statistics is to summarize and describe data.")
 })
