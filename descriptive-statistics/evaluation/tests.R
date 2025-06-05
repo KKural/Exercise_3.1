fc95f9e6c7de80146b24d63529f2eda91f8a3c79
KKural <122997366+KKural@users.noreply.github.com>
2025-06-05 16:17:53 +0200

    Delete descriptive-statistics/evaluation/tests.R

--- descriptive-statistics/evaluation/tests.R
+++
@@ -1,6 +0,0 @@
-context("Check correct answer for multiple choice")
-
-test_that("Answer must be option 2", {
-  expect_equal(eval(parse(text = readLines("main.R"))), 2,
-               info = "Incorrect: The main purpose of descriptive statistics is to summarize and describe data.")
-})
