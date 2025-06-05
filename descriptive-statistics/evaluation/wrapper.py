211b603deba6c7c604812945f5b4c9099b112980
KKural <122997366+KKural@users.noreply.github.com>
2025-06-05 16:48:29 +0200

    Delete descriptive-statistics/evaluation/wrapper.py

--- descriptive-statistics/evaluation/wrapper.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import sys, importlib.util, pathlib
-
-student_file = pathlib.Path(sys.argv[1])
-
-spec = importlib.util.spec_from_file_location("student", student_file)
-student = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(student)
