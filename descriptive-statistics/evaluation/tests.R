6738e821390b891a7cd52bf0502e448119ad1726
Kural <kuralarasan.kumar@ugent.be>
2025-06-07 17:03:08 +0200

    refactor: update evaluation handler and simplify tests in configuration and test files

--- descriptive-statistics/evaluation/tests.R
+++ descriptive-statistics/evaluation/tests.R
@@ -1,8 +1,3 @@
 context({
-  testcase("echo(\"test\")", {
-    testEqual(NULL, function(env) { env$echo("test") }, 'test')
-  })
-  testcase("echo(\"test2\")", {
-    testEqual(NULL, function(env) { env$echo("test2") }, 'test2')
-  })
+    testcaseAssert('abc exists', function(studentEnv) { isTRUE(exists("abc", studentEnv)) })
 })
