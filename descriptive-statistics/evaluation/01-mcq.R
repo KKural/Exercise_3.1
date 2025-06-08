a1a1b025a4ec91b2079356f93c45be8afdf8d25d
Kural <kuralarasan.kumar@ugent.be>
2025-06-08 06:33:44 +0200

    refactor: update testcase label for clarity

--- descriptive-statistics/evaluation/01-mcq.R
+++ descriptive-statistics/evaluation/01-mcq.R
@@ -1,9 +1,10 @@
 # context block 2: the actual MCQ
 context({
-  testcase("Test", {
+  testcase(" ", {
     # `evaluationResult` is the value of the last top-level expression
     # in the student script (so if they write `2`, evaluationResult == 2).
     testEqual(
+      " ",                       # label shown to student
       function(env) env$evaluationResult,
       2                               # the correct numeric choice
     )
