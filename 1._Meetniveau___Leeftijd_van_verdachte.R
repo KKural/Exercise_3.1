e567ef346619e7dd2867cf312315a4d937e92378
Kural <kuralarasan.kumar@ugent.be>
2025-10-10 16:58:33 +0200

    Remove redundant input handling from age evaluation exercise and add new central research question activity with answer evaluation.

--- 1._Meetniveau___Leeftijd_van_verdachte.R
+++ 1._Meetniveau___Leeftijd_van_verdachte.R
@@ -1,9 +1,3 @@
 /* https://dodona.be/nl/courses/5541/series/65553/activities/982511611 */
 
-# Read the input (student's answer)
-student_answer <- as.numeric(readLines("stdin", n=1))
-
-# Output the student's answer for evaluation
-cat(student_answer)
-
 
